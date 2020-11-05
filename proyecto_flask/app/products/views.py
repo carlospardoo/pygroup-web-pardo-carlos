c600c07ef4a4ee6ef18c48e71e045917277b6245
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-11-04 22:37:20 -0500

    Agregada consulta. Pendiente README.md

--- proyecto_flask/app/products/views.py
+++ proyecto_flask/app/products/views.py
@@ -1,6 +1,33 @@
 """
 Consulta método render_template
 
+Método utilizado para renderizar como salida de una ruta, una plantilla 
+(página web) almacenada en un archivo. Flask utiliza por defecto el motor 
+de plantillas jinja2.
+
+Argumentos: 
+    Nombre de la plantilla.
+    Variables dinámicas enviadas como argumentos adicionales.
+
+render_template ingresará a la carpeta templates y buscará la plantilla.
+Si la aplicación es un módulo, la carpeta templates irá al lado del módulo.
+    /aplicacion.py
+    /templates
+        /hello.html
+Si la aplicación es un paquete, la carpeta templates irá dentro del paquete.
+    /aplicacion
+        /__init__.py
+        /templates
+            /hello.html
+
+El método render_template se importa de la siguiente manera:
+    from flask import render_template
+
+Se utiliza así:
+    @render.route("/")
+    def ejemplo():
+        variable = [7,4,3,2]
+        return render_template("hola.html",vars=variable)
 """
 
 from flask import Blueprint, Response
